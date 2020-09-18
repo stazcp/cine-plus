65ac812e59a01faaa7a061afcdc9f1c7d271fd7e
Arvin Behshad <arvin@gimitheapp.com>
2020-09-18 18:13:15 +0200

    feedback

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer';
 import AppBar from './components/AppBar';
 
 function App() {
+  // TODO: This is where you would do your routing to decide where the links in AppBar go
+  // ideally also not straight in App, but create a new wrapper component for it
   return (
     <>
       <AppBar />
