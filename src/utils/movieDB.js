df3fc695ebb6753aab3c5c4ab7a71e958de5b4d0
stazcp <staz.cp@gmail.com>
2020-09-29 23:56:00 -0700

    security update

--- src/utils/movieDB.js
+++ src/utils/movieDB.js
@@ -2,12 +2,8 @@
 //   --url 'https://api.themoviedb.org/3/movie/76341' \
 //   --header 'Authorization: Bearer <<access_token>>' \
 //   --header 'Content-Type: application/json;charset=utf-8'
+import {api_key} from '../keys.json';
 
-// let session_id;
-// const access_token =
-//   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDQ2ZDQ0ZjdmOGYyMTcwNDMwNTA0ODQ4MGQyZjEyOCIsInN1YiI6IjVlZjIzYmZlOWE4YThhMDAzNWI0MTcxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4_xLuZujLXBRRSRjCrOy2kso4DvOLuRJGDPFQGjx3ao';
-// const cors_url = 'https://cors-anywhere.herokuapp.com/';
-export const api_key = '6d46d44f7f8f21704305048480d2f128';
 const base_url = 'https://api.themoviedb.org/3/';
 
 export const getConfig = async () => {
