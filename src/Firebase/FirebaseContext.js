91ae587716b54d8e9d3ba59c6f52718014299f7d
stazcp <staz.cp@gmail.com>
2020-11-08 14:39:59 -0800

    refactoring

--- src/Firebase/FirebaseContext.js
+++ src/Firebase/FirebaseContext.js
@@ -39,186 +39,127 @@ const provider = new firebase.auth.GoogleAuthProvider()
 // composition
 export function FirebaseProvider({ children }) {
   firebase.auth().onAuthStateChanged((user) => setUser(user))
-  const [user, setUser] = useState(),
-    // item can be person, movie, or show
-    favorite = async (eleId, type) => {
-      if (user) {
-        try {
-          const docRef = db.collection('users').doc(user.email),
-            doc = await docRef.get()
-          if (!doc.exists) {
-            return false
-          }
-          if (type === 'movie') {
-            const { favoriteMovies } = doc.data()
-            await docRef.set(
-              {
-                favoriteMovies: [...favoriteMovies, eleId],
-              },
-              { merge: true }
-            )
-          } else if (type === 'tv') {
-            const { favoriteShows } = doc.data()
-            await docRef.set(
-              {
-                favoriteShows: [...favoriteShows, eleId],
-              },
-              { merge: true }
-            )
-          } else if (type === 'person') {
-            const { favoriteActors } = doc.data()
-            await docRef.set(
-              {
-                favoriteActors: [...favoriteActors, eleId],
-              },
-              { merge: true }
-            )
-          }
-        } catch (error) {
-          console.log(error)
-          return false
-        }
-      } else {
-        console.log('no user')
-        return false
-      }
-      return true
-    },
-    //if successful will return false to remove the liked state
-    removeFavorite = async (eleId, type) => {
-      if (user) {
-        try {
-          const docRef = db.collection('users').doc(user.email),
-            doc = await docRef.get()
-          if (!doc.exists) {
-            return true
-          }
-          if (type === 'movie') {
-            const { favoriteMovies } = doc.data()
-            let index = favoriteMovies.findIndex((movie) => movie === eleId)
-            if (index !== -1) {
-              favoriteMovies.splice(index, 1)
-              await docRef.set(
-                {
-                  favoriteMovies: favoriteMovies,
-                },
-                { merge: true }
-              )
-              return false
-            } else {
-              return true
-            }
-          } else if (type === 'tv') {
-            const { favoriteShows } = doc.data()
-            let index = favoriteShows.findIndex((movie) => movie === eleId)
-            if (index !== -1) {
-              favoriteShows.splice(index, 1)
-              await docRef.set(
-                {
-                  favoriteShows: favoriteShows,
-                },
-                { merge: true }
-              )
-              return false
-            } else {
-              return true
-            }
-          } else if (type === 'person') {
-            const { favoriteActors } = doc.data()
-            let index = favoriteActors.findIndex((movie) => movie === eleId)
-            if (index !== -1) {
-              favoriteActors.splice(index, 1)
-              await docRef.set(
-                {
-                  favoriteActors: favoriteActors,
-                },
-                { merge: true }
-              )
-              return false
-            } else {
-              return true
-            }
-          }
-        } catch (error) {
-          console.log(error)
-          return true
-        }
-      }
-      console.log('no user')
-      return true
-    },
-    checkLiked = async (eleId, type) => {
-      if (user) {
-        try {
-          const docRef = db.collection('users').doc(user.email)
-          const doc = await docRef.get()
-          if (!doc.exists) {
-            return false
-          }
-          if (type === 'movie') {
-            const { favoriteMovies } = doc.data()
-            let found = favoriteMovies.filter((movie) => movie === eleId)
-            if (found.length) {
-              return true
-            } else {
-              return false
-            }
-          } else if (type === 'tv') {
-            const { favoriteShows } = doc.data()
-            let found = favoriteShows.filter((movie) => movie === eleId)
-            if (found.length) {
-              return true
-            } else {
-              return false
-            }
-          } else if (type === 'person') {
-            const { favoriteActors } = doc.data()
-            let found = favoriteActors.filter((movie) => movie === eleId)
-            if (found.length) {
-              return true
-            } else {
-              return false
-            }
-          }
-        } catch (error) {
-          console.log(error)
-          return false
-        }
-      } else {
+  const [user, setUser] = useState()
+
+  const transformType = (type) => {
+    switch (type) {
+      case 'movie':
+        return 'favoriteMovies'
+      case 'tv':
+        return 'favoriteShows'
+      case 'person':
+        return 'favoriteActors'
+      default:
         return null
-      }
-      return null
-    },
-    //add new user to database and set schema
-    newUser = async (email) => {
+    }
+  }
+
+  // item can be person, movie, or show
+  const favorite = async (eleId, type) => {
+    if (user) {
       try {
-        const docRef = db.collection('users').doc(email)
-        await docRef.set({
-          favoriteMovies: [],
-          favoriteShows: [],
-          favoriteActors: [],
-        })
-      } catch (error) {}
-    },
-    getFavorites = async (type) => {
-      if (user) {
         const docRef = db.collection('users').doc(user.email)
         const doc = await docRef.get()
-        switch (type) {
-          case 'movie':
-            const { favoriteMovies } = doc.data()
-            return favoriteMovies
-          case 'tv':
-            const { favoriteShows } = doc.data()
-            return favoriteShows
-          case 'person':
-            const { favoriteActors } = doc.data()
-            return favoriteActors
-          default:
-            return null
+        if (!doc.exists) {
+          return false
         }
+        type = transformType(type)
+        let data = await doc.data()[type]
+        await docRef.set(
+          {
+            [type]: [...data, eleId],
+          },
+          { merge: true }
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    } else {
+      console.log('no user')
+      return false
+    }
+    return false
+  }
+
+  //if successful will return false to remove the liked state
+  const removeFavorite = async (eleId, type) => {
+    if (user) {
+      try {
+        const docRef = db.collection('users').doc(user.email)
+        const doc = await docRef.get()
+        if (!doc.exists) {
+          return true
+        }
+        type = transformType(type)
+        let data = await doc.data()[type]
+        let index = data.findIndex((movie) => movie === eleId)
+        if (index === -1) {
+          return true
+        }
+        data.splice(index, 1)
+        await docRef.set(
+          {
+            [type]: data,
+          },
+          { merge: true }
+        )
+        return false
+      } catch (error) {
+        console.log(error)
+        return true
       }
     }
+    console.log('no user')
+    return true
+  }
+
+  const checkLiked = async (eleId, type) => {
+    if (user) {
+      try {
+        const docRef = db.collection('users').doc(user.email)
+        const doc = await docRef.get()
+        if (!doc.exists) {
+          return false
+        }
+        type = transformType(type)
+        let data = await doc.data()[type]
+        let found = data.filter((movie) => movie === eleId)
+        if (!found.length) {
+          return false
+        }
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    } else {
+      return null
+    }
+    return null
+  }
+
+  //add new user to database and set schema
+  const newUser = async (email) => {
+    try {
+      const docRef = db.collection('users').doc(email)
+      await docRef.set({
+        favoriteMovies: [],
+        favoriteShows: [],
+        favoriteActors: [],
+      })
+    } catch (error) {}
+  }
+
+  const getFavorites = async (type) => {
+    if (user) {
+      const docRef = db.collection('users').doc(user.email)
+      const doc = await docRef.get()
+      type = transformType(type)
+      return doc.data()[type]
+    }
+  }
 
   return (
     <FirebaseContext.Provider
